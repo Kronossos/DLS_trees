4deef015748e18506ebb6b2d047315bda08dfe0a
Kronossos <alek10000@gmail.com>
2019-03-12 12:39:10 +0100

    Scenario reader updated. Test scenarios added.

--- reader.py
+++ reader.py
@@ -3,12 +3,10 @@
 
 
 import sys
-
-files = sys.argv[1:]
-
+import collections
 
 class TreeNode:
-    def __init__(self, id: int,change_id: tuple,change_type: tuple, duplication_prefix: tuple):
+    def __init__(self, id: int, change_id: tuple, change_type: tuple, duplication_prefix: tuple):
         self.id = id
         self.duplication_prefix = duplication_prefix
 
@@ -17,20 +15,56 @@ class TreeNode:
         # Tmove=1, Clost=0
         # {1: [list of trees], 0: [list of trees]}
         # or {tree: connection_type,  tree_2: connection_type, ...} <---- current
+        # Store ID or object?
         for i in range(len(change_id)):
-            connections[change_id[i]]=change_type[i]
+            connections[change_id[i]] = change_type[i]
+        self.connections = connections
 
-
-
-
+    def __str__(self):
+        return str(self.id) + ":" + str(self.duplication_prefix)
 
 
 class Scenario:
     def __init__(self, filename):
-        nodes = {}
-
+        nodes = collections.OrderedDict()
         with open(filename, "r") as scenario_file:
             for line in scenario_file:
-                id, change, change_type, duplication_prefix = line.split("\t")
-                new_tree = TreeNode(int(id), tuple(change), tuple(change_type), tuple(duplication_prefix))
-                nodes[id]=new_tree
+                id, change, change_type, duplication_prefix = line.strip().split(" ") # tab,space or comma?
+                new_tree = TreeNode(int(id), eval(change), eval(change_type), eval(duplication_prefix))
+                nodes[id] = new_tree
+        self.nodes = nodes
+
+    def __iter__(self):
+        for tree_id in reversed(self.nodes):
+            yield self.nodes[tree_id]
+
+    def __str__(self):
+        pass
+
+
+class AllScenarios:
+    def __init__(self, files_list):
+        scenarios = []
+        for file in files_list:
+            scenarios.append(Scenario(file))
+        self.scenarios = scenarios
+
+    def __iter__(self):
+        for scenario in self.scenarios:
+            yield scenario
+
+    def __str__(self):
+        pass
+
+
+def test():
+    # files = sys.argv[1:]
+    files = ["scenario_1", "scenario_2"]
+    a=AllScenarios(files)
+    for sc in a:
+        print()
+        for tree in sc:
+            print(tree)
+
+
+test()
